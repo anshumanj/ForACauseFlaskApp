639f6a48981933844e0c50e66f22964f0062cb6e
Alex A <alex.av936@gmail.com>
2020-07-06 21:25:38 -0500

    Maybe if I take out this comment, it won't be confused.

--- queryFunctions.py
+++ queryFunctions.py
@@ -6,7 +6,6 @@ from boto3.dynamodb.conditions import Key
 car = CarKeys()
 
 def register_nonProfit(id, name, email, category, tagline, mission, website):
-    #dynamodb_client_local = boto3.client("dynamodb", endpoint_url="http://localhost:8000")
     dynamodb_client_cloud = boto3.client("dynamodb", region_name="us-east-2")
 
     companyInfo = {
@@ -33,7 +32,6 @@ def register_nonProfit(id, name, email, category, tagline, mission, website):
 
 
 def GetNextId(tableName):
-    #dynamodb_client_local = boto3.client("dynamodb", endpoint_url="http://localhost:8000")
     dynamodb_client_cloud = boto3.client("dynamodb", region_name="us-east-2")
 
     lastItem = dynamodb_client_cloud.scan(
